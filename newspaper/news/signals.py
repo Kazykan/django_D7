d744e06102832709ff21db22312f496fdf5f60fe
Kazykan <rufat@kocherbaev.ru>
2021-12-04 13:29:27 +0300

    celery_app

--- newspaper/news/signals.py
+++ newspaper/news/signals.py
@@ -13,15 +13,21 @@ def notify_subscribers_publication(instance, created, **kwargs):
     if not created:
         subject = f'Изменена публикация: {instance.headingPost} -- {instance.timeInCreation.strftime("%d %m %Y")}'
         cat_id_list = list(PostCategory.objects.filter(postThrough_id=instance.id).values('categoryThrough_id'))
-        list_of_subscribers = []
+        list_of_subscribers_all = []
 
         for cat in cat_id_list:
             cat_id = cat.get('categoryThrough_id')
             filtered_subscribers = list(CategorySub.objects.filter(categorySub_id=cat_id).values('subscriberUser_id__email'))
-            list_of_subscribers.append([d['subscriberUser_id__email'] for d in filtered_subscribers if 'subscriberUser_id__email' in d][0])
 
-        html = render_to_string('mailings/mailings_create.html', {'form': instance, 'instance': instance.id})
-        sending_an_email(subject, html, list_of_subscribers)
+            list_of_subscribers_to_the_category = add_list_of_subscribers(filtered_subscribers)
+            if list_of_subscribers_to_the_category:
+                list_of_subscribers_all += list_of_subscribers_to_the_category
+
+        list_of_subscribers_all = list(set(list_of_subscribers_all))  # убираем дубликаты
+
+        if list_of_subscribers_all:
+            html = render_to_string('mailings/mailings_create.html', {'form': instance, 'instance': instance.id})
+            sending_an_email(subject, html, list_of_subscribers_all)
 
 
 @receiver(m2m_changed, sender=Post.postCategory.through)
@@ -29,14 +35,28 @@ def notify_subscribers_publication(instance, **kwargs):
     """сигнал проверки связей ManyToMany на изменения связей в посте через цикл прогоняем все pk_set записей
      находим через categorySub_id все email добавляем их в список и запускаем ф-цию отправки писем"""
     subject = f'Добавлена публикация: {instance.headingPost} -- {instance.timeInCreation.strftime("%d %m %Y")}'
-    list_of_subscribers = []
+    list_of_subscribers_all = []
 
     for cat_id in list(kwargs.get('pk_set')):
         filtered_subscribers = list(CategorySub.objects.filter(categorySub_id=cat_id).values('subscriberUser_id__email'))
-        list_of_subscribers.append([d['subscriberUser_id__email'] for d in filtered_subscribers if 'subscriberUser_id__email' in d][0])
 
-    html = render_to_string('mailings/mailings_create.html', {'form': instance, 'instance': instance.id})
-    sending_an_email(subject, html, list_of_subscribers)
+        list_of_subscribers_to_the_category = add_list_of_subscribers(filtered_subscribers)
+        if list_of_subscribers_to_the_category:
+            list_of_subscribers_all += list_of_subscribers_to_the_category
+
+    list_of_subscribers_all = list(set(list_of_subscribers_all))  # убираем дубликаты
+
+    if list_of_subscribers_all:
+        html = render_to_string('mailings/mailings_create.html', {'form': instance, 'instance': instance.id})
+        sending_an_email(subject, html, list_of_subscribers_all)
+
+
+def add_list_of_subscribers(filtered_subscribers):
+    """из списка удаляем ключь 'subscriberUser_id__email' и оставляем только email
+    [{'subscriberUser_id__email': 'email@mail.ru'}, {'subscriberUser_id__email': 'email@mail.com'}]"""
+    list_of_subscription_categories = [d['subscriberUser_id__email']
+                                       for d in filtered_subscribers if 'subscriberUser_id__email' in d]
+    return list_of_subscription_categories
 
 
 def sending_an_email(subject, html, list_of_subscribers):
